366c3ae93883659a695bd373ae5324805807ec2d
Zvonimir Belina <zbelina20@foi.hr>
2024-06-04 21:52:47 +0200

    Kreiran početni projekt.

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'none'),
 
     /*
     |--------------------------------------------------------------------------
@@ -107,6 +107,12 @@ return [
             'prefix_indexes' => true,
             // 'encrypt' => env('DB_ENCRYPT', 'yes'),
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
+        ],
+
+        'none' => [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
         ],
 
     ],
